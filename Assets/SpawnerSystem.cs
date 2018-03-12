42162327c3dae08ce6ce53a4b5796e37fea2663e
Keijiro Takahashi <keijiro@gmail.com>
2018-03-12 17:52:05 +0900

    implementation

--- Assets/SpawnerSystem.cs
+++ Assets/SpawnerSystem.cs
@@ -3,58 +3,74 @@ using Unity.Transforms;
 using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Rendering;
-using UnityEngine;
 
 class SpawnerSystem : ComponentSystem
 {
-    struct Data
+    struct SpawnerData
     {
         public Entity entity;
         public float3 position;
+        public quaternion rotation;
         public int count;
     }
 
-    private ComponentGroup m_Group;
-    
+    private ComponentGroup m_SpawnerGroup;
+    private EntityArchetype m_SpinnerArchetype;
+
     protected override void OnCreateManager(int capacity)
     {
-        m_Group = GetComponentGroup(typeof(Spawner), typeof(Position));
+        m_SpawnerGroup = GetComponentGroup(typeof(Spawner), typeof(Position), typeof(Rotation));
+        m_SpinnerArchetype = EntityManager.CreateArchetype(
+            typeof(Position), typeof(Rotation), typeof(TransformMatrix),
+            typeof(Spinner), typeof(MeshInstanceRendererComponent));
     }
 
     protected override void OnUpdate()
     {
-        var entities = m_Group.GetEntityArray();
-        var positions = m_Group.GetComponentDataArray<Position>();
-        var spawners = m_Group.GetComponentDataArray<Spawner>();
+        var spawnerCount = m_SpawnerGroup.CalculateLength();
+        var data = new NativeArray<SpawnerData>(spawnerCount, Allocator.Temp);
 
-        var data = new NativeArray<Data>(spawners.Length, Allocator.Temp);
-
-        for (var i = 0; i != spawners.Length; i++)
         {
-            data[i] = new Data
+            var entities = m_SpawnerGroup.GetEntityArray();
+            var positions = m_SpawnerGroup.GetComponentDataArray<Position>();
+            var rotations = m_SpawnerGroup.GetComponentDataArray<Rotation>();
+            var spawners = m_SpawnerGroup.GetComponentDataArray<Spawner>();
+
+            for (var i = 0; i != spawnerCount; i++)
             {
-                entity = entities[i],
-                position = positions[i].Value,
-                count = spawners[i].count
-            };
+                data[i] = new SpawnerData
+                {
+                    entity = entities[i],
+                    position = positions[i].Value,
+                    rotation = rotations[i].Value,
+                    count = spawners[i].count
+                };
+            }
         }
 
         var renderer = UnityEngine.GameObject.Find("Cube").GetComponent<MeshInstanceRendererComponent>().Value;
+        var seed = 0;
 
-        for (var i = 0; i != data.Length; i++)
+        for (var i = 0; i != spawnerCount; i++)
         {
-            var instance = EntityManager.CreateEntity();
-            EntityManager.AddComponent(instance, typeof(Position));
-            EntityManager.AddComponent(instance, typeof(Spinner));
-            EntityManager.SetComponentData(instance, new Position { Value = 0 });
-            EntityManager.SetComponentData(instance, new Spinner { seed = 123 });
+            var instance = EntityManager.CreateEntity(m_SpinnerArchetype);
+            EntityManager.SetComponentData(instance, new Position {Value = data[i].position});
+            EntityManager.SetComponentData(instance, new Rotation {Value = data[i].rotation});
+            EntityManager.SetComponentData(instance, new Spinner {seed = seed++});
             EntityManager.AddSharedComponentData(instance, renderer);
-            EntityManager.AddComponentData(instance, new TransformMatrix { Value = Matrix4x4.identity });
-            
-            var clones = new NativeArray<Entity>(data[i].count, Allocator.Temp);
-            EntityManager.Instantiate(instance, clones);
-            clones.Dispose();
-            
+
+            var cloneCount = data[i].count - 1;
+            if (cloneCount > 0)
+            {
+                var clones = new NativeArray<Entity>(cloneCount, Allocator.Temp);
+                EntityManager.Instantiate(instance, clones);
+
+                for (var offs = 0; offs < cloneCount; offs++)
+                    EntityManager.SetComponentData(clones[offs], new Spinner {seed = seed++});
+
+                clones.Dispose();
+            }
+
             EntityManager.RemoveComponent(data[i].entity, typeof(Spawner));
         }
 
--- Assets/SpinnerSystem.cs
+++ Assets/SpinnerSystem.cs
@@ -8,13 +8,21 @@ class SpinnerSystem : ComponentSystem
     struct Data
     {
         public int Length;
-        [ReadOnly] public ComponentDataArray<Position> Positions;
         [ReadOnly] public ComponentDataArray<Spinner> Spinners;
+        public ComponentDataArray<Position> Positions;
+        public ComponentDataArray<Rotation> Rotations;
     }
 
     [Inject] private Data m_Data;
 
     protected override void OnUpdate()
     {
+        for (var i = 0; i < m_Data.Length; i++)
+        {
+            m_Data.Rotations[i] = new Rotation
+            {
+                Value = math.mul(m_Data.Rotations[i].Value, math.axisAngle(new float3(0, 1, 0), 0.1f))
+            };
+        }
     }
 }
