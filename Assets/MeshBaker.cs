0bb024a20c774a2a7ad6b7471f9183b78b4f4eec
Keijiro Takahashi <keijiro@gmail.com>
2018-03-18 21:45:55 +0900

    implementation

---
+++ Assets/MeshBaker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MeshBaker : MonoBehaviour
+{
+    [SerializeField] SkinnedMeshRenderer _source;
+    [SerializeField] MeshCollider _target;
+
+    Mesh _mesh;
+
+    void Start()
+    {
+        _mesh = new Mesh();
+    }
+
+    void Update()
+    {
+        transform.position = _source.transform.position;
+        transform.rotation = _source.transform.rotation;
+
+        _source.BakeMesh(_mesh);
+        _target.sharedMesh = _mesh;
+    }
+}
